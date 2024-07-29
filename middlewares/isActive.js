f1c0f541f156d8dce56ac8c54ef242e8bc5042a3
Ali Hazer <alihazerr28@gmail.com>
2024-07-29 17:18:04 +0300

    User status

--- middlewares/isActive.js
+++ middlewares/isActive.js
@@ -2,10 +2,11 @@
 
 export default function isActive(req, res, next) {
     const user = req.cookies.user;
-    console.log("User: ", user);
+    console.log("User: ", user.status);
     if (user.status == "active") {
         next();
     } else {
+        console.log("User is not active");
         res.status(403).redirect('/login');
     }
 }
