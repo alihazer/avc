61c1538c4b42d1d7ca9a7f352864bbfa01820f92
Ali Hazer <alihazerr28@gmail.com>
2024-08-04 11:34:22 +0300

    Added Loading spinner

--- public/firebase/borrowItem.js
+++ public/firebase/borrowItem.js
@@ -9,42 +9,51 @@ document.getElementById('materialForm').addEventListener('submit', async (event)
     const imageInput = document.getElementById('image');
     const form = event.target;
     const hidden = document.getElementById('hidden');
-    
-
+    const loadingSpinner = document.getElementById('loadingSpinner');
+    // Show loading spinner
+    loadingSpinner.style.display = 'block';
     if (name === '') {
         showAlert('Name is required');
+        loadingSpinner.style.display = 'none';
         return;
     }
     if(!phone_nb){
         showAlert('Phone number is required');
+        loadingSpinner.style.display = 'none';
         return;
     }
     if (!imageFile) {  
         showAlert('Image is required');
+        loadingSpinner.style.display = 'none';
         return;
     }
 
     try {
         const imageURL = await uploadImage(imageFile, "borrowed-items-onBorrow-images");
-       
+        
         hidden.value = imageURL;
 
         // Set form action and method
         form.method = 'POST';
+        // Hide loading spinner
+        loadingSpinner.style.display = 'none';
         // Submit the form
         form.submit();
     } catch (error) {
         console.error(error);
         showAlert('An error occurred');
+        // Hide loading spinner
+        loadingSpinner.style.display = 'none';
     }
 });
-
 
 const showAlert = (message) => {
     const alert = document.getElementById('alert');
     const alertMessage = document.getElementById('alertMessage');
     alertMessage.textContent = message;
     alert.style.display = 'block';
+    // Hide loading spinner
+    loadingSpinner.style.display = 'none';
 };
 
 document.getElementById('closeBtn').addEventListener('click', () => {
--- public/firebase/returnItem.js
+++ public/firebase/returnItem.js
@@ -1,6 +1,5 @@
 import { uploadImage } from './imageUpload.js';
 
-
 document.getElementById('materialForm').addEventListener('submit', async (event) => {
     event.preventDefault();
     const url = new URL(window.location.href).pathname.split('/');
@@ -15,7 +14,7 @@ document.getElementById('materialForm').addEventListener('submit', async (event)
     const error = document.getElementById('alert');
     const imageInput = document.getElementById('image');
     const notes = document.getElementById('notes');
-    
+    const loadingSpinner = document.getElementById('loadingSpinner');
 
     // Reset error message
     alertMessage.innerText = '';
@@ -28,14 +27,18 @@ document.getElementById('materialForm').addEventListener('submit', async (event)
         return;
     }
 
+    // Show loading spinner
+    loadingSpinner.style.display = 'flex';
+
     try {
         const imageURL = await uploadImage(imageFile, "borrowed-items-images");
-        
+
         // Create the data object
         const data = {
             imageOnReturn: imageURL,
             notes: notes.value
         };
+
         // Make the PUT request
         fetch(`/borrowed-items/return/${id}`, {
             method: 'PUT',
@@ -47,6 +50,9 @@ document.getElementById('materialForm').addEventListener('submit', async (event)
         })
         .then(res => res.json())
         .then(data => {
+            // Hide loading spinner
+            loadingSpinner.style.display = 'none';
+
             if (data.status) {
                 window.location.href = '/borrowed-items/category/' + url[3] + '/items';
             } else {
@@ -55,6 +61,8 @@ document.getElementById('materialForm').addEventListener('submit', async (event)
         })
         .catch(err => {
             showAlert('An error occurred');
+            // Hide loading spinner
+            loadingSpinner.style.display = 'none';
         });
 
         // Reset the file input value
@@ -64,6 +72,8 @@ document.getElementById('materialForm').addEventListener('submit', async (event)
         console.error('Error uploading file:', error);
         alertMessage.innerText = 'Error uploading file';
         error.style.display = 'block';
+        // Hide loading spinner
+        loadingSpinner.style.display = 'none';
     }
 });
 
@@ -72,6 +82,9 @@ const showAlert = (message) => {
     const alertMessage = document.getElementById('alertMessage');
     alertMessage.textContent = message;
     alert.style.display = 'block';
+    // Hide loading spinner
+    const loadingSpinner = document.getElementById('loadingSpinner');
+    loadingSpinner.style.display = 'none';
 };
 
 document.getElementById('closeBtn').addEventListener('click', () => {
