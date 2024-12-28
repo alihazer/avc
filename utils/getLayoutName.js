78ea8ff3e0e1a691e43a20e29ca8045c05cdd11c
Ali Hazer <alihazerr28@gmail.com>
2024-12-28 14:39:21 +0200

    TT

--- utils/getLayoutName.js
+++ utils/getLayoutName.js
@@ -1,12 +1,10 @@
 const getLayoutName = (req)=>{
     const accessibles = ['admin', 'superadmin'];
     const noAccess = ['driver', 'stockmanager', 'shiftmanager'];
-    let role;
-    try {
-        role = JSON.parse(req?.cookies?.user)?.role?.name;
-    } catch (error) {
-        console.log(error);
-    }
+    // if user in the cookies is no json, parse it
+    const user = typeof req.cookies.user === 'string' ? JSON.parse(req.cookies.user) : req.cookies.user;
+    console.log('user', user);
+    const role = user ? user?.role?.name : null;
     if(!role){
         return 'layouts/loginLayout';
     }
